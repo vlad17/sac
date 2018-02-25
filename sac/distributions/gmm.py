238c1a815dab75e6e63ef161ad536e55a2cf80c5
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2018-02-25 09:57:16 -0800

    rm debug prints

--- sac/distributions/gmm.py
+++ sac/distributions/gmm.py
@@ -6,6 +6,7 @@ import numpy as np
 from sac.misc.mlp import mlp
 
 LOG_SIG_CAP_MAX = 2
+LOG_SIG_CAP_MIN = -10
 
 
 class GMM(object):
@@ -37,27 +38,15 @@ class GMM(object):
     @staticmethod
     def _create_log_gaussian(mu_t, log_sig_t, t):
         abm = lambda x: tf.reduce_mean(tf.abs(x))
-        mu_t = mu_t
-        mu_t = tf.Print(mu_t, [abm(mu_t)], 'mu_t')
-        log_sig_t = log_sig_t
-        log_sig_t = tf.Print(log_sig_t, [abm(log_sig_t), tf.reduce_min(log_sig_t), tf.reduce_max(log_sig_t)], 'log_sig_t,min,max')
-        t = t
-        t = tf.Print(t, [abm(t)], 't')
         normalized_dist_t = (t - mu_t) * tf.exp(-log_sig_t)  # ... x D
-        normalized_dist_t = tf.Print(normalized_dist_t, [abm(normalized_dist_t)], 'normalized_dist_t')        
         quadratic = - 0.5 * tf.reduce_sum(normalized_dist_t ** 2, axis=-1)
-        quadratic = tf.Print(quadratic, [abm(quadratic)], 'quadratic')
         # ... x (None)
 
         log_z = tf.reduce_sum(log_sig_t, axis=-1)  # ... x (None)
-        log_z = tf.Print(log_z, [abm(log_z)], 'log_z')
         D_t = tf.cast(tf.shape(mu_t)[-1], tf.float32)
-        D_t = tf.Print(D_t, [abm(D_t)], 'D_t')
         log_z += 0.5 * D_t * np.log(2 * np.pi)
-        log_z = tf.Print(log_z, [abm(log_z)], 'log_z+')
 
         log_p = quadratic - log_z
-        log_p = tf.Print(log_p, [abm(log_p)], 'log_p')
 
         return log_p  # ... x (None)
 
@@ -86,6 +75,7 @@ class GMM(object):
         log_sig_t = w_and_mu_and_logsig_t[..., 1+Dx:]
 
         log_sig_t = tf.minimum(log_sig_t, LOG_SIG_CAP_MAX)
+        log_sig_t = tf.maximum(log_sig_t, LOG_SIG_CAP_MIN)
 
         return log_w_t, mu_t, log_sig_t
 
